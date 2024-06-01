0230fc3b9de818677ae124d6340cdbcad3cd5a49
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-06-01 16:30:36 +0100

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  const categories = ["Sport", "Tech", "Beauty", "Clothes"];
+
+  const displayCategories = categories.map((category, index) => (
+    <li key={index}>
+      <a href="" className="capitalize">{category}</a>
+    </li>
+  ));
+
+  return (
+    <header className="bg-primary-green">
+      <div className="container">
+        <nav className="h-24 flex justify-between items-center text-neutral-white">
+          <a href="">Store App</a>
+          <ul className="nav-list flex gap-5">{displayCategories}</ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
